Zeus-Deus/hermes-agent

fix(desktop): routed fresh chat keeps its exact owner after session.create
A fresh chat created through $newChatRoute lost its owner the moment
session.create returned. The create RPC rode the captured route
(requestGatewayForAgent), but the optimistic row was stamped from
$activeGatewayProfile — still `default` in All-profiles / Bot routing —
and no owner hint was recorded. The first turn ran on the routed
backend (e.g. local::omar); every later session-scoped RPC resolved the
row as `default` and 4001'd "session not found", leaving the routed
runtime to be ws-orphan-reaped.

Make the ownership transition atomic with the create:
- record capturedRoute as the stored session's exact owner hint the
  moment a routed create returns a stored id (main chat and tile paths);
- upsertOptimisticSession accepts an explicit owner and stamps
  profile = targetProfile || profile plus the owning connection_id,
  falling back to the ambient profile only for an unrouted create;
- contrib/wiring resolves session RPC owners as: persisted tile owner
  route → exact unique owner hint → session-row profile → cross-profile
  probe (resolveSessionRpcOwner, pure + unit-tested), so prompt.submit,
  session.resume, attachments, interrupt, redirect and recovery all use
  the exact owner; knownOwnerForSession follows the same ladder;
- the mid-create drift-abort session.close rides capturedRoute too.

Regressions: an integration test (ambient default, route local::omar,
two turns → both prompt.submit hit local::omar, no session-not-found,
no session.close) and a unit test asserting a routed fresh create never
yields { followupOwner: 'default', foregroundScope: 'conn:local::omar' }.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>
(cherry picked from commit 95e2ef66becb641d3bdedf016dab6c104a777ae7)

apps/desktop/src/app/contrib/wiring-routing.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
-import { findStoredIdForRuntimeId, resolveRoutingSessionId } from './wiring-routing'
+import { findStoredIdForRuntimeId, resolveRoutingSessionId, resolveSessionRpcOwner } from './wiring-routing'
 
 describe('findStoredIdForRuntimeId', () => {
   it('reverse-resolves a runtime id to its stored id', () => {
@@ -74,5 +74,67 @@ describe('resolveRoutingSessionId', () => {
         storedIdForRuntime: never
       })
     ).toBeNull()
+  })
+})
+
+describe('resolveSessionRpcOwner', () => {
+  const none = () => undefined
+  const omar = { connectionId: 'local', mode: 'local' as const, profile: 'omar' }
+  const homelab = { connectionId: 'homelab', mode: 'remote' as const, profile: 'worker', targetProfile: 'w' }
+
+  it('returns undefined for an RPC with no session (ambient chrome)', () => {
+    expect(
+      resolveSessionRpcOwner({
+        routingSessionId: null,
+        sessionOwnerHint: none,
+        sessionRowProfile: none,
+        tileOwnerRoute: none
+      })
+    ).toBeUndefined()
+  })
+
+  it('prefers the persisted tile owner route over the hint and the row', () => {
+    const owner = resolveSessionRpcOwner({
+      routingSessionId: 'stored-bot',
+      sessionOwnerHint: () => omar,
+      sessionRowProfile: () => 'default',
+      tileOwnerRoute: () => homelab
+    })
+
+    expect(owner).toEqual(homelab)
+  })
+
+  it('prefers the exact unique owner hint over the session row profile', () => {
+    // The row is presentation state: an optimistic row minted while the
+    // ambient profile stayed `default` reads `default` even though the create
+    // ran on local::omar. The hint recorded at create time is exact.
+    const owner = resolveSessionRpcOwner({
+      routingSessionId: 'stored-omar',
+      sessionOwnerHint: id => (id === 'stored-omar' ? omar : undefined),
+      sessionRowProfile: () => 'default',
+      tileOwnerRoute: none
+    })
+
+    expect(owner).toEqual(omar)
+  })
+
+  it('falls back to the session row profile, then to undefined for the probe', () => {
+    expect(
+      resolveSessionRpcOwner({
+        routingSessionId: 'stored-1',
+        sessionOwnerHint: none,
+        sessionRowProfile: () => 'coder',
+        tileOwnerRoute: none
+      })
+    ).toBe('coder')
+
+    expect(
+      resolveSessionRpcOwner({
+        routingSessionId: 'stored-1',
+        sessionOwnerHint: none,
+        sessionRowProfile: () => '  ',
+        tileOwnerRoute: none
+      })
+    ).toBeUndefined()
   })
 })

apps/desktop/src/store/session-states.ts
@@ -42,6 +42,7 @@ import {
   $selectedStoredSessionId,
   $sessions,
   clearReadBaseline,
+  getSessionOwnerHint,
   knownSessionProfile,
   lineageAliases,
   markSessionRead,
@@ -742,8 +743,12 @@ export function sessionTileOwnerRoute(storedSessionId: string): SessionProfileRo
 /**
  * Sync owner resolution for a session id that may be a RUNTIME or a STORED id.
  * Tile route first (exact connectionId+profile, survives relaunch), then the
- * known session profile (row or open-time hint). Returns undefined when no
- * owner is known — the caller falls back to ambient, never to "active".
+ * exact unique owner hint (stamped when a routed create returns / at open
+ * time), then the known session profile (row, else hint). The hint outranks
+ * the row for the same reason as contrib/wiring's ladder: a row can be
+ * stamped from the ambient profile and carries no connection. Returns
+ * undefined when no owner is known — the caller falls back to ambient, never
+ * to "active".
  */
 export function knownOwnerForSession(sessionId: null | string | undefined): SessionOwnerScope {
   if (!sessionId) {
@@ -752,7 +757,11 @@ export function knownOwnerForSession(sessionId: null | string | undefined): Sess
 
   const storedSessionId = storedSessionIdForRuntimeId(sessionId) ?? sessionId
 
-  return sessionTileOwnerRoute(storedSessionId) ?? knownSessionProfile($sessions.get(), storedSessionId)
+  return (
+    sessionTileOwnerRoute(storedSessionId) ??
+    getSessionOwnerHint(storedSessionId) ??
+    knownSessionProfile($sessions.get(), storedSessionId)
+  )
 }
 
 /**

qxxaa/hermes-mobile

apps/desktop/src/app/contrib/wiring-routing.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
-import { findStoredIdForRuntimeId, resolveRoutingSessionId } from './wiring-routing'
+import { findStoredIdForRuntimeId, resolveRoutingSessionId, resolveSessionRpcOwner } from './wiring-routing'
 
 describe('findStoredIdForRuntimeId', () => {
   it('reverse-resolves a runtime id to its stored id', () => {
@@ -74,5 +74,67 @@ describe('resolveRoutingSessionId', () => {
         storedIdForRuntime: never
       })
     ).toBeNull()
+  })
+})
+
+describe('resolveSessionRpcOwner', () => {
+  const none = () => undefined
+  const omar = { connectionId: 'local', mode: 'local' as const, profile: 'omar' }
+  const homelab = { connectionId: 'homelab', mode: 'remote' as const, profile: 'worker', targetProfile: 'w' }
+
+  it('returns undefined for an RPC with no session (ambient chrome)', () => {
+    expect(
+      resolveSessionRpcOwner({
+        routingSessionId: null,
+        sessionOwnerHint: none,
+        sessionRowOwner: none,
+        tileOwnerRoute: none
+      })
+    ).toBeUndefined()
+  })
+
+  it('prefers the persisted tile owner route over the hint and the row', () => {
+    const owner = resolveSessionRpcOwner({
+      routingSessionId: 'stored-bot',
+      sessionOwnerHint: () => omar,
+      sessionRowOwner: () => 'default',
+      tileOwnerRoute: () => homelab
+    })
+
+    expect(owner).toEqual(homelab)
+  })
+
+  it('prefers the exact unique owner hint over the session row profile', () => {
+    // The row is presentation state: an optimistic row minted while the
+    // ambient profile stayed `default` reads `default` even though the create
+    // ran on local::omar. The hint recorded at create time is exact.
+    const owner = resolveSessionRpcOwner({
+      routingSessionId: 'stored-omar',
+      sessionOwnerHint: id => (id === 'stored-omar' ? omar : undefined),
+      sessionRowOwner: () => 'default',
+      tileOwnerRoute: none
+    })
+
+    expect(owner).toEqual(omar)
+  })
+
+  it('falls back to the session row profile, then to undefined for the probe', () => {
+    expect(
+      resolveSessionRpcOwner({
+        routingSessionId: 'stored-1',
+        sessionOwnerHint: none,
+        sessionRowOwner: () => 'coder',
+        tileOwnerRoute: none
+      })
+    ).toBe('coder')
+
+    expect(
+      resolveSessionRpcOwner({
+        routingSessionId: 'stored-1',
+        sessionOwnerHint: none,
+        sessionRowOwner: () => '  ',
+        tileOwnerRoute: none
+      })
+    ).toBeUndefined()
   })
 })

apps/desktop/src/store/session-states.ts
@@ -43,6 +43,7 @@ import {
   $selectedStoredSessionId,
   $sessions,
   clearReadBaseline,
+  getSessionOwnerHint,
   knownSessionOwner,
   lineageAliases,
   markSessionRead,
@@ -844,8 +845,12 @@ export function openTileGatewayScopes(): Set<string> {
 /**
  * Sync owner resolution for a session id that may be a RUNTIME or a STORED id.
  * Tile route first (exact connectionId+profile, survives relaunch), then the
- * known session owner (row or open-time hint). Returns undefined when no
- * owner is known — the caller falls back to ambient, never to "active".
+ * exact unique owner hint (stamped when a routed create returns / at open
+ * time), then the known session owner (a connection-tagged row's exact route,
+ * else its profile / the hint). The hint outranks the row for the same reason
+ * as contrib/wiring's ladder: a row can be stamped from the ambient profile
+ * and carries no connection. Returns undefined when no owner is known — the
+ * caller falls back to ambient, never to "active".
  */
 export function knownOwnerForSession(sessionId: null | string | undefined): SessionOwnerScope {
   if (!sessionId) {
@@ -854,7 +859,11 @@ export function knownOwnerForSession(sessionId: null | string | undefined): Sess
 
   const storedSessionId = storedSessionIdForRuntimeId(sessionId) ?? sessionId
 
-  return sessionTileOwnerRoute(storedSessionId) ?? knownSessionOwner($sessions.get(), storedSessionId)
+  return (
+    sessionTileOwnerRoute(storedSessionId) ??
+    getSessionOwnerHint(storedSessionId) ??
+    knownSessionOwner($sessions.get(), storedSessionId)
+  )
 }
 
 /**